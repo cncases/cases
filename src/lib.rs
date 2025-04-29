0776a0d30752184d3a0634455af84e27196f3b73
cncases <156569830+cncases@users.noreply.github.com>
2025-04-29 02:00:07 +0000

    update

--- src/lib.rs
+++ src/lib.rs
@@ -30,35 +30,35 @@ pub fn kv_sep_partition_option() -> PartitionCreateOptions {
 
 #[derive(Debug, Encode, Decode, Serialize, Deserialize)]
 pub struct Case {
-    #[serde(rename(deserialize = "原始链接"))]
+    #[serde(rename = "原始链接")]
     pub doc_id: String,
-    #[serde(rename(deserialize = "案号"))]
+    #[serde(rename = "案号")]
     pub case_id: String,
-    #[serde(rename(deserialize = "案件名称"))]
+    #[serde(rename = "案件名称")]
     pub case_name: String,
-    #[serde(rename(deserialize = "法院"))]
+    #[serde(rename = "法院")]
     pub court: String,
-    // #[serde(rename(deserialize = "所属地区"))]
+    // #[serde(rename = "所属地区")]
     // pub region: String,
-    #[serde(rename(deserialize = "案件类型"))]
+    #[serde(rename = "案件类型")]
     pub case_type: String,
-    // #[serde(rename(deserialize = "案件类型编码"))]
+    // #[serde(rename = "案件类型编码")]
     // case_type_code: String,
-    // #[serde(rename(deserialize = "来源"))]
+    // #[serde(rename = "来源")]
     // source: String,
-    #[serde(rename(deserialize = "审理程序"))]
+    #[serde(rename = "审理程序")]
     pub procedure: String,
-    #[serde(rename(deserialize = "裁判日期"))]
+    #[serde(rename = "裁判日期")]
     pub judgment_date: String,
-    #[serde(rename(deserialize = "公开日期"))]
+    #[serde(rename = "公开日期")]
     pub public_date: String,
-    #[serde(rename(deserialize = "当事人"))]
+    #[serde(rename = "当事人")]
     pub parties: String,
-    #[serde(rename(deserialize = "案由"))]
+    #[serde(rename = "案由")]
     pub cause: String,
-    #[serde(rename(deserialize = "法律依据"))]
+    #[serde(rename = "法律依据")]
     pub legal_basis: String,
-    #[serde(rename(deserialize = "全文"))]
+    #[serde(rename = "全文")]
     pub full_text: String,
 }
 
